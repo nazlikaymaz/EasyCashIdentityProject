644e3bfc3a1dc197224ac07227e128b814056cac
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2023-08-22 17:32:15 +0300

    AppUserRegisterDto ve AppUserRegister Fluent Validation

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı adı boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter girişi yapın");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalarınız eşleşmiyor");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir mail adresi giriniz");
+        }
+    }
+}
--- EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -12,6 +12,8 @@ namespace EasyCashIdentityProject.EntityLayer.Concrete
         public string CustomerAccountNumber { get; set; }
         public string CustomerAccountCurrency { get; set; }
         public decimal CustomerAccountBalance { get; set; }
-        public string BankBranch{ get; set; }
+        public string BankBranch { get; set; }
+        public int AppUserID { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
