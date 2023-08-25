51aa2d963a08d0b182c1dc85377b0f5bbd6acccc
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2023-08-26 00:37:19 +0300

    maile gelen kodu onaylama ve mail aktivasyonu

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -60,7 +60,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
 
                     SmtpClient client = new SmtpClient();
                     client.Connect("smtp.gmail.com", 587, false);
-                    client.Authenticate("852deneme@gmail.com", "oshdljpgbnrikwtk");
+                    client.Authenticate("852deneme@gmail.com", "vsnlvtpxjhwpnibd");
                     client.Send(mimeMessage);
                     client.Disconnect(true);
 
